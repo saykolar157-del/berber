6d28b9bd6302b414f335d09d826d340c4ba87ef1
saykolar157-del <saykolar157@gmail.com>
2026-05-23 21:23:05 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,16 @@
 <?php
 
 $conn = new mysqli(
-$_ENV["railway"],
-$_ENV["root"],
-$_ENV["RgRNUVdPMMAtvQqwgnqoBYtPcBZWLIDO"],
-$_ENV["railway"],
-$_ENV["3306"]
+getenv("MYSQLHOST"),
+getenv("MYSQLUSER"),
+getenv("MYSQLPASSWORD"),
+getenv("MYSQLDATABASE"),
+getenv("MYSQLPORT")
 );
 
-if($conn->connect_error){
-die("DB HATA: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("DB HATA: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
-
 ?>
