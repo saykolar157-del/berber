86d65dd26a7bb4e094c22cbe537e65e021134a77
saykolar157-del <saykolar157@gmail.com>
2026-05-23 21:31:11 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,23 @@
 <?php
 
-$host = $_SERVER['MYSQLHOST'] ?? getenv("MYSQLHOST");
-$user = $_SERVER['MYSQLUSER'] ?? getenv("MYSQLUSER");
-$pass = $_SERVER['MYSQLPASSWORD'] ?? getenv("MYSQLPASSWORD");
-$db   = $_SERVER['MYSQLDATABASE'] ?? getenv("MYSQLDATABASE");
-$port = $_SERVER['MYSQLPORT'] ?? getenv("MYSQLPORT");
+$host = "kodama.proxy.rlwy.net";
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = 31819;
 
-if(!$host || !$user || !$pass || !$db){
-    die("ENV GELMİYOR → Railway variable attach sorunu");
-}
-
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conn = new mysqli(
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port
+);
 
 if ($conn->connect_error) {
     die("DB HATA: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
+
 ?>
