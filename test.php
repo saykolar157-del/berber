4b7c89ba9bcfeeb498a79d8b7c445ff3738fcc4e
saykolar157-del <saykolar157@gmail.com>
2026-05-23 21:37:54 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+var_dump(getenv("MYSQLHOST"));
+var_dump(getenv("MYSQLUSER"));
+var_dump(getenv("MYSQLPASSWORD"));
+var_dump(getenv("MYSQLDATABASE"));
+var_dump(getenv("MYSQLPORT"));
+exit;
+?>
