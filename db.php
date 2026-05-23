9c0113b9a94bfc560e39db7c540c372d523ec938
saykolar157-del <saykolar157@gmail.com>
2026-05-23 21:28:45 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,16 @@
 <?php
 
-$conn = new mysqli(
-getenv("MYSQLHOST"),
-getenv("MYSQLUSER"),
-getenv("MYSQLPASSWORD"),
-getenv("MYSQLDATABASE"),
-getenv("MYSQLPORT")
-);
+$host = $_SERVER['MYSQLHOST'] ?? getenv("MYSQLHOST");
+$user = $_SERVER['MYSQLUSER'] ?? getenv("MYSQLUSER");
+$pass = $_SERVER['MYSQLPASSWORD'] ?? getenv("MYSQLPASSWORD");
+$db   = $_SERVER['MYSQLDATABASE'] ?? getenv("MYSQLDATABASE");
+$port = $_SERVER['MYSQLPORT'] ?? getenv("MYSQLPORT");
+
+if(!$host || !$user || !$pass || !$db){
+    die("ENV GELMİYOR → Railway variable attach sorunu");
+}
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("DB HATA: " . $conn->connect_error);
