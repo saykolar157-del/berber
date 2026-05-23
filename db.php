17ad627b7def65a529323e2501cd6b63c2742a41
saykolar157-del <saykolar157@gmail.com>
2026-05-23 22:02:40 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,21 +1,16 @@
 <?php
 
-$host = $_ENV['MYSQLHOST'] ?? $_SERVER['MYSQLHOST'] ?? getenv('MYSQLHOST');
-$user = $_ENV['MYSQLUSER'] ?? $_SERVER['MYSQLUSER'] ?? getenv('MYSQLUSER');
-$pass = $_ENV['MYSQLPASSWORD'] ?? $_SERVER['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD');
-$db   = $_ENV['MYSQLDATABASE'] ?? $_SERVER['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
-$port = $_ENV['MYSQLPORT'] ?? $_SERVER['MYSQLPORT'] ?? getenv('MYSQLPORT');
-
-if(!$host || !$user || !$pass || !$db){
-    die("ENV GELMİYOR → Railway Variable Reference bağlanmamış");
-}
-
-$conn = new mysqli($host, $user, $pass, $db, (int)$port);
+$conn = new mysqli(
+  getenv("DB_HOST"),
+  getenv("DB_USER"),
+  getenv("DB_PASS"),
+  getenv("DB_NAME"),
+  3306
+);
 
 if ($conn->connect_error) {
-    die("DB HATA: " . $conn->connect_error);
+  die("DB HATA: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
-
 ?>
