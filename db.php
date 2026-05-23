72153143de18775b3321772b476b0e733bd964c9
saykolar157-del <saykolar157@gmail.com>
2026-05-23 21:33:11 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,16 @@
 <?php
 
-$host = "kodama.proxy.rlwy.net";
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = 31819;
+$host = $_ENV['MYSQLHOST'] ?? $_SERVER['MYSQLHOST'] ?? getenv('MYSQLHOST');
+$user = $_ENV['MYSQLUSER'] ?? $_SERVER['MYSQLUSER'] ?? getenv('MYSQLUSER');
+$pass = $_ENV['MYSQLPASSWORD'] ?? $_SERVER['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD');
+$db   = $_ENV['MYSQLDATABASE'] ?? $_SERVER['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
+$port = $_ENV['MYSQLPORT'] ?? $_SERVER['MYSQLPORT'] ?? getenv('MYSQLPORT');
 
-$conn = new mysqli(
-    $host,
-    $user,
-    $pass,
-    $db,
-    $port
-);
+if(!$host || !$user || !$pass || !$db){
+    die("ENV GELMİYOR → Railway Variable Reference bağlanmamış");
+}
+
+$conn = new mysqli($host, $user, $pass, $db, (int)$port);
 
 if ($conn->connect_error) {
     die("DB HATA: " . $conn->connect_error);
